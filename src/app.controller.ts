f869c6484cf1e622379087e0a7db2766d9c6c58c
Wellington Brandão <wellington.brandao@simbiox.com.br>
2023-12-17 18:54:29 -0300

    fix:add / endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { TextConvertDto } from './dto/textConvert.dto';
 
 @Controller('/api/v1')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Get('/')
+  getHome() {
+    return 'API/V1';
+  }
 
   @Post('/text/convertPdf')
   async receiveText(@Body() textConvertDto: TextConvertDto): Promise<void> {
